c73afe782e44327cf31991febaa6c150f90540c7
Armando <armandosaenzm@gmail.com>
2025-05-27 21:15:36 -0500

    Added dockerfiles for mongo database and node server, added docker-compose and use them as services. Added mongoose connection, schema and model

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express';
+import SnippetController from './controllers/snippet';
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,9 @@ const port = 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
+
+//Register Controllers
+SnippetController(app);
 
 app.listen(port, () => {
 console.log(`Server running at http://localhost:${port}`);
